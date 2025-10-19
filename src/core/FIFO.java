56926dfffa32272a5dd7ac8bcf0045b53a964440
Joao-ava <ajoao781238@gmail.com>
2025-10-19 20:02:25 -0300

    add: FIFO base structure

---
+++ src/core/FIFO.java
@@ -0,0 +1,15 @@
+package core;
+
+public class FIFO {
+    private int[] pages;
+    private int partitions;
+
+    public FIFO(int[] pages, int partitions) {
+        this.pages = pages;
+        this.partitions = partitions;
+    }
+
+    public int execute() {
+        return 0;
+    }
+}

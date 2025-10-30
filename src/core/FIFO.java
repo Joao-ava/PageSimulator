40e59d9fb245fc4ada3decce7dd54d2731a928a6
taismoreira2023 <projetos174@gmail.com>
2025-10-30 16:07:33 -0300

    add: getMemoryList in FIFO

--- src/core/FIFO.java
+++ src/core/FIFO.java
@@ -1,15 +1,24 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FIFO {
     private int[] pages;
     private int partitions;
+    private final List<MemoryState> memoryList;
 
     public FIFO(int[] pages, int partitions) {
         this.pages = pages;
         this.partitions = partitions;
+        this.memoryList = new ArrayList<>();
     }
 
     public int execute() {
         return 0;
+    }
+
+    public List<MemoryState> getMemoryList() {
+        return this.memoryList;
     }
 }
